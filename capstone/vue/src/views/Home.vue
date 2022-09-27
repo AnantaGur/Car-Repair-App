4684042cea12f9d7794a9bc80451e8474cad8c91
Ananta Gurung <grgananta99@gmail.com>
2022-09-27 13:25:59 -0400

    Repair Request page and Home page edits

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <div class="container">
+      <img src="../images/black-logo.png" alt="banner" class="banner">
+    <table>
+      <th>SERVICES</th><br>
+      <tr>Estimated Schedule <td>This is an estimate on your schedule</td></tr><br>
+      <tr>Estimated Cost <td>This is an estimate on your cost</td></tr>
+    </table>
+    <router-link :to="{name: 'repair'}" class="link">Repair Request</router-link>
+  </div>
   </div>
 </template>
 
@@ -10,3 +17,48 @@ export default {
   name: "home"
 };
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap");
+
+.home {
+  font-family: "Dosis", sans-serif;
+}
+.container {
+  display: flex;
+  align-items: center;
+  flex-flow: column;
+}
+.banner {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  width: 25%;
+  background-color: black;
+  padding-left: 100%;
+  padding-right: 100%;
+}
+
+.link {
+  text-decoration: none;
+  color: black;  
+  margin-top: 50px;
+  font-size: 32px;
+}
+
+.link:hover {
+  background-color: teal;
+  color: white;
+  border-radius: 10px;
+  padding: 2px 16px 4px 16px;
+  box-shadow: 10px 5px black;
+}
+
+table {
+  display: flex;
+  flex-flow: column;
+  margin-top: 10vh;
+  font-size: 32px;
+}
+
+</style>
---
+++ capstone/vue/src/views/Repair.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="repair-contain">
+      <h1>Repair Form</h1>
+      <form action="repairform.txt">
+          <label for="vname">Vehicle Name:</label>
+          <input type="text" id="vname" name="vname">
+          <label for="vmodel">Vehicle Model:</label>
+          <input type="text" id="vmodel" name="vmodel">
+          <label for="vcolor">Vehicle Color:</label>
+          <input type="text" id="vcolor" name="vcolor">
+          <label for="oname">Owner Name:</label>
+          <input type="text" id="oname" name="oname">
+          <label for="pnumber">Phone Number:</label>
+          <input type="text" id="pnumber" name="pnumber">
+          <input type="submit" value="submit" id="submit">
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "repair"
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap");
+
+.repair-contain {
+ font-family: "Dosis", sans-serif;
+}
+
+#submit {
+    margin-top: 15px;
+    
+}
+
+form label {
+    margin: 10px;
+}
+
+h1 {
+    display: flex;
+    justify-content: center;
+}
+</style>
