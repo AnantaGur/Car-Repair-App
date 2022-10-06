866c4bd840e99ecf86c34febdd34db7e082f74cf
Gary Luna <garyluna92@gmail.com>
2022-10-06 09:39:16 -0500

    updated mock data

--- capstone/java/database/insertIntoTables.sql
+++ capstone/java/database/insertIntoTables.sql
@@ -1,23 +1,23 @@
 insert into users
 (username, password_hash, role)
 VALUES
---password 123
+--username: fran, password: 123
 ('fran', '$2a$10$sK06AA0Ln.Mz7kWPYHavPeenEUN9jmhVR9QiZJjYPS3jI2q1fuXjW', 'ROLE_ADMIN');
 
 insert into users
 (username, password_hash, role)
---password 1234 the users is abc
+--username: gary, password: 1234
 VALUES
 ('GARY', '$2a$10$kCcVff5m/CLUS3Yp1uffTejFUrS1fbnDwXAS.YFfC0A/pIrSjY7ou', 'ROLE_EMPLOYEE');
 
 
 insert into users
---password 12345 user is newuser
+--username: newuser, password 12345
 (username, password_hash, role)
 VALUES
 ('Ananta', '$2a$10$L48qsnx0ghnGS0VMr7OCu.59KUXWka53CEespckCvCDT8Z33KWnlC', 'ROLE_USER');
 
---password 123456 user is newuser2
+--username: newuser2, password 123456
 insert into users
 (username, password_hash, role)
 VALUES
@@ -46,7 +46,7 @@ VALUES
 insert into service_request
 (vehicle_make, vehicle_model, vehicle_color, vehicle_year, service_name, full_name, phone_number, email, user_id)
 VALUES
-('nissan', 'xterra', 'grey', 2010, 'Battery Change', 'Gary', '1234567890', 'nissan@gmail.com', 4);
+('nissan', 'xterra', 'grey', 2010, 'Battery Change', 'Gary', '1234567890', 'nissan@gmail.com', 5);
 
 --insert into repair_estimate
 --(request_id, parts_cost, labor_cost, total_time, pick_up_date, pick_up_time)
